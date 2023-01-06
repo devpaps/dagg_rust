c2e3fcbdee4d3ae3bd70004481bf5885f053ab55
devpaps <crymeat@gmail.com>
2023-01-06 09:21:22 +0100

    Added Chakra-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,11 @@
-import type { AppProps } from "next/app";
-import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme/customTheme';
 
-// import "../style.css";
-// import "../App.css";
-
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS={true} theme={theme}>
-      <Component {...pageProps} />;
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
