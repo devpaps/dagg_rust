2e5634dc3e85bab097bb82dcf40984ee7dd31f7e
devpaps <crymeat@gmail.com>
2023-01-09 16:48:10 +0100

    Added some beginning of a sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { AiOutlineHome } from 'react-icons/ai';
+import { Box, HStack, Link, Text } from '@chakra-ui/react';
 
 export default function Sidebar() {
   return (
-    <Box width="300px" borderRight="1px solid gray">
-      <Text>Navbar</Text>
+    <Box height="100vh" borderRight="1px solid gray">
+      <Text>Dagg</Text>
+      <HStack verticalAlign="bottom">
+        <AiOutlineHome />
+        <Link href="/">Home</Link>
+      </HStack>
     </Box>
   );
 }
