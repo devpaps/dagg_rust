f02219ac9dc05872daa2883651e374eebb5141ae
devpaps <crymeat@gmail.com>
2023-01-09 16:49:05 +0100

    Cna now show a proper icon of the weather and user dont need to press Sök button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,15 @@ import { invoke } from '@tauri-apps/api/tauri';
 import {
   Box,
   Button,
-  Container,
   Flex,
+  Grid,
   Heading,
+  HStack,
   Input,
   Text,
   VStack,
 } from '@chakra-ui/react';
+import Image from 'next/image';
 import SideBar from '../components/Sidebar';
 
 interface Data {
@@ -17,7 +19,7 @@ interface Data {
   current: {
     temp: number;
     humidity: number;
-    weather: { description: string }[];
+    weather: { description: string; icon: number }[];
   };
   error_message?: string;
 }
@@ -30,49 +32,81 @@ function App() {
     setGreetMsg(await invoke('get_data', { city: name }));
   }
 
+  function handleChange(e: any) {
+    e.preventDefault();
+    setName(e.currentTarget.value);
+    greet();
+  }
+
+  console.log(greetMsg);
+
   return (
-    <>
+    <Grid templateColumns="300px 1fr">
       <Box>
         <SideBar />
       </Box>
       <Flex justifyContent="center">
         <Flex>
           <Box>
-            <Heading>Välkommen till Dagg!</Heading>
+            <Box mt={20}>
+              <Heading>Dagens väder</Heading>
+            </Box>
 
-            <Text>Ange en svensk stad för att se det aktuella vädret</Text>
-
-            <VStack>
+            <VStack mt={20}>
+              <Text fontWeight="medium">
+                Ange en svensk stad för att se det aktuella vädret
+              </Text>
               <Box>
-                <Input
-                  id="greet-input"
-                  onChange={(e) => setName(e.currentTarget.value)}
-                  placeholder="Ex. Stockholm"
-                />
-                <Button type="button" onClick={() => greet()}>
-                  Hämta
-                </Button>
+                <form onSubmit={handleChange}>
+                  <Input
+                    id="greet-input"
+                    size="lg"
+                    mt={4}
+                    onChange={(e) => setName(e.currentTarget.value)}
+                    placeholder="Ex. Stockholm"
+                  />
+                  <Button
+                    type="submit"
+                    width="100%"
+                    mt={5}
+                    size="lg"
+                    onClick={() => greet()}
+                  >
+                    Hämta
+                  </Button>
+                </form>
               </Box>
             </VStack>
             {greetMsg ? (
               <Flex direction="column">
                 {greetMsg.error_message ? (
-                  <Text>{greetMsg.error_message}</Text>
+                  <Text textAlign="center" mt="10" fontWeight="bold">
+                    {greetMsg.error_message}
+                  </Text>
                 ) : (
-                  <>
+                  <Box mt={10}>
                     <Text>{`Temperatur: ${greetMsg.current.temp.toFixed(
                       1,
                     )} °C`}</Text>
                     <Text>{`Fuktighet: ${greetMsg.current.humidity} %`}</Text>
-                    <Text>{greetMsg.current.weather[0].description}</Text>
-                  </>
+                    <HStack>
+                      <Image
+                        objectFit="contain"
+                        width="50px"
+                        height="50px"
+                        src={`http://openweathermap.org/img/w/${greetMsg.current.weather[0].icon}.png`}
+                        alt="weathericon"
+                      />
+                      <Text>{greetMsg.current.weather[0].description}</Text>
+                    </HStack>
+                  </Box>
                 )}
               </Flex>
             ) : null}
           </Box>
         </Flex>
       </Flex>
-    </>
+    </Grid>
   );
 }
 
