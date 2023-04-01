13eb9a92786b89f1ea0d4228ea06e784bf3cd6b8
devpaps <crymeat@gmail.com>
2023-04-01 08:39:54 +0200

    Switch to using double quotes consistently in customTheme.ts for consistency with the rest of the project

--- src/theme/customTheme.ts
+++ src/theme/customTheme.ts
@@ -1,18 +1,18 @@
-import { extendTheme, ThemeConfig } from '@chakra-ui/react';
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
-  initialColorMode: 'dark',
+  initialColorMode: "dark",
   useSystemColorMode: false,
 };
 
 const colors = extendTheme({
   colors: {
     brand: {
-      100: '#f7fafc',
-      900: '#1a202c',
+      100: "#f7fafc",
+      900: "#1a202c",
     },
     background: {
-      100: '#181818',
+      100: "#181818",
     },
   },
 });
